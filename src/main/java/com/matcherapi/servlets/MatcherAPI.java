366c0574a3a697d869f3b21d10b8a076e10ba189
David <drs@a.recife.ifpe.edu.br>
2021-02-13 11:56:42 -0300

    Adiciona código fonte

---
+++ src/main/java/com/matcherapi/servlets/MatcherAPI.java
@@ -0,0 +1,32 @@
+package com.matcherapi.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "MatcherAPI", urlPatterns = {"/MatcherAPI"})
+public class MatcherAPI extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        response.setContentType("application/json");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        try ( PrintWriter out = response.getWriter()) {
+
+            out.println("{status: '200', token:'AKSJDA98012IJDNAO8127HDABS',  }");
+        }
+    }
+}
